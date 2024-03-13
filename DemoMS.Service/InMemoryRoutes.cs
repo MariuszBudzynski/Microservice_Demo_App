ff4c1760b75f42256c80b6e8e0b89f7a1fa31e1a
Hood <mariusz.budzynski87@gmail.com>
2024-03-13 11:53:32 +0100

    As i will implement the DB context later i moved the Routes that use List in memory to another class for simpler switch later on

---
+++ DemoMS.Service/InMemoryRoutes.cs
@@ -0,0 +1,37 @@
+﻿using DemoMS.Service.DTOS;
+using DemoMS.Service.Repository.InMemory.InMemoryUseCases.Interfaces;
+using DemoMS.Service.Repository.InMemory.UseCases.Interfaces;
+
+namespace DemoMS.Service
+{
+    public static class InMemoryRoutes
+    {
+        public static void ConfigureInMemoryRoutes(WebApplication app)
+        {
+            app.MapGet("/items", (IInMemoryGetAllIUseCase<ItemDto> inMemoryGetAllIUseCase) =>
+            {
+                return inMemoryGetAllIUseCase.Execute();
+            });
+
+            app.MapGet("/items/{id}", (Guid id, IInMemoryGetDataByIDUseCase<ItemDto> inMemoryGetDataByIDUseCase) =>
+            {
+                return inMemoryGetDataByIDUseCase.Execute(id);
+            });
+
+            app.MapPost("/items", (ItemDto item, IInMemoryAddDataUseCase<ItemDto> inMemoryAddDataUseCase) =>
+            {
+                inMemoryAddDataUseCase.Execute(item);
+            });
+
+            app.MapPut("/items{id}", (ItemDto data, IInMemoryUpdateDataUseCase<ItemDto> inMemoryUpdateDataUseCase) =>
+            {
+                inMemoryUpdateDataUseCase.Execute(data);
+            });
+
+            app.MapDelete("/items{id}", (Guid id, IInMemoryDeleteDataUseCase<ItemDto> inMemoryDeleteDataUseCase) =>
+            {
+                return inMemoryDeleteDataUseCase.Execute(id);
+            });
+        }
+    }
+}
--- DemoMS.Service/Program.cs
+++ DemoMS.Service/Program.cs
@@ -1,7 +1,4 @@
 using DemoMS.Service;
-using DemoMS.Service.DTOS;
-using DemoMS.Service.Repository.InMemory.InMemoryUseCases.Interfaces;
-using DemoMS.Service.Repository.InMemory.UseCases.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -23,30 +20,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.MapGet("/items", (IInMemoryGetAllIUseCase<ItemDto> inMemoryGetAllIUseCase) =>
-{
-    //return Results.Ok(inMemoryGetAllIUseCase.Execute());
-    return inMemoryGetAllIUseCase.Execute();
-});
-
-app.MapGet("/items/{id}", (Guid id,IInMemoryGetDataByIDUseCase<ItemDto> inMemoryGetDataByIDUseCase) =>
-{
-    return inMemoryGetDataByIDUseCase.Execute(id);
-});
-
-app.MapPost("/items", (ItemDto item,IInMemoryAddDataUseCase<ItemDto> inMemoryAddDataUseCase) =>
-{
-    inMemoryAddDataUseCase.Execute(item);
-});
-
-app.MapPut("/items{id}", (ItemDto data,IInMemoryUpdateDataUseCase<ItemDto> inMemoryUpdateDataUseCase) =>
-{
-    inMemoryUpdateDataUseCase.Execute(data);
-});
-
-app.MapDelete("/items{id}", (Guid id,IInMemoryDeleteDataUseCase<ItemDto> inMemoryDeleteDataUseCase) =>
-{
-    return inMemoryDeleteDataUseCase.Execute(id);
-});
+InMemoryRoutes.ConfigureInMemoryRoutes(app);
 
 app.Run();
